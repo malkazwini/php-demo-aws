034c768af591f8fb6d66a9a86da8946bd99842eb
user <kalkazwinI@deloitte.ca>
2017-08-27 14:58:15 -0400

    added uptime

--- src/index.php
+++ src/index.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Simple PHP App</title>
+        <title>Deloitte Pipeline Demo</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="assets/css/bootstrap.min.css" rel="stylesheet">
         <style>body {margin-top: 40px; background-color: #333;}</style>
@@ -16,13 +16,20 @@
         <div class="container">
             <div class="hero-unit">
                 <h1>Simple PHP App via pipeline</h1>
-                <h2>Congratulations</h2>
+                <h2>Congratulations!!! You pushed a new change</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
                         echo fread($myfile,filesize("/var/www/my-vol/date"));
                         fclose($myfile);
+                ?>
+                <?php 
+                echo '<b>Uptime:</b><br><br>';
+                echo 'Days: '.$uptime_days.'<br>';
+                echo 'Hours: '.$uptime_hours.'<br>';
+                echo 'Minutes: '.$uptime_minutes.'<br>';
+                echo 'Seconds: '.$uptime_seconds.'<br>';
                 ?>
 
             </div>
