3e73de47e6ca2e85aeae733f40be776d72d5be63
user <kalkazwinI@deloitte.ca>
2017-08-27 21:40:50 -0400

    init

--- src/index.php
+++ src/index.php
@@ -1,29 +1,70 @@
 <!DOCTYPE html>
 <html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+    <meta name="description" content="Meta descriptions are HTML attributes that provide concise explanations of the contents of web pages. They should optimally be between 150-160 characters.">
+    <meta name="author" content="SvenCreations">
 
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Simple PHP App</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-        <style>body {margin-top: 40px; background-color: #333;}</style>
-        <link href="assets/css/bootstrap-responsive.min.css" rel="stylesheet">
-        <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-    </head>
+    <!-- Touch icons for android and iOS (Bookmark icons) -->
+    <link rel="shortcut icon" sizes="192x192" href="images/icons/android-icon-192x192.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="images/icons/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="images/icons/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="images/icons/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="images/icons/apple-icon-152x152.png">
 
-    <body>
-        <div class="container">
-            <div class="hero-unit">
-                <h1>Simple PHP App</h1>
-                <h2>Congratulations!</h2>
-                <p>Your PHP application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in your own dedicated environment in the AWS&nbsp;Cloud.</p>
-                <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
+    <!-- Schema.org markup for Google+ and facebook -->
+    <!-- <meta property="og:title" content="Title should be concise">
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="http://your-website-url.com"/>
+    <meta property="og:image" content="Use images that are at least 1200 x 630 pixels for the best display on high resolution devices">
+    <meta property="og:description" content="A description that concisely summarizes the content of the page"> -->
+
+    <!-- Schema.org markup for twitter cards(summary_large_image) -->
+    <!-- <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@username">
+    <meta name="twitter:title" content="Title should be concise and will be truncated at 70 characters.">
+    <meta name="twitter:description" content="A description that concisely summarizes the content of the page, as appropriate for presentation within a Tweet. Do not re-use the title text as the description, or use this field to describe the general services provided by the website. Description text will be truncated at the word to 200 characters.">
+    <meta name="twitter:image" content="URL to a unique image representing the content of the page. Do not use a generic image such as your website logo, author photo, or other image that spans multiple pages. Images for this Card should be at least 280px in width, and at least 150px in height. Image must be less than 1MB in size."> -->
+
+    <title>Pipeline Demo</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:400">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body id="minimos-1">
+
+    <div class="preloader abs-center">
+        <div class="spinner"></div>
+    </div>
+
+    <div class="content-wrapper">
+        <div class="v-center">
+            <div class="main-content">
+                <p>Your PHP application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in your own dedicated environment in the AWS&nbsp;CodePipeline on ECS. This Docker container is running PHP version <?php echo phpversion(); ?>.</p>
+                <ul>
+                    <li><a href="http://twitter.com/">Twitter</a></li>
+                    <li><a href="http://facebook.com/">Facebook</a></li>
+                    <li><a href="https://dribbble.com/">Dribbble</a></li>
+                    <li><a href="mailto:kalkazwini@deloitte.ca">Contact</a></li>
+                </ul>
             </div>
         </div>
+    </div>
 
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-        <script src="assets/js/bootstrap.min.js"></script>
-    </body>
+    <!-- =================================================================
+    COMPILED JS PLUGINS
+    ================================================================== -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.2/TweenMax.min.js"></script>
+    <script src="/assets/js/dependencies.js"></script>
+    <script src="/assets/js/main.js"></script>
 
+</body>
 </html>
